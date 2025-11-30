d186dfd042c3c3f4632ced93d92e891e92c55e81
hailtotheley <tanzhailey@gmail.com>
2025-11-30 10:21:55 +0800

    asked my brother to try out the app here are the tips of the features he missed

--- Skys The Limit/Core/Tip.swift
+++ Skys The Limit/Core/Tip.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 import TipKit
 
-struct Tippy: Tip {
-    var title: Text {
-        Text("Tippyity")
-    }
-    var message: Text? {
-        Text("Add new tip")
-    }
-    var image: Image? {
-        Image(systemName: "paintpalette")
-    }
-    
-    var rules: [Rule] {
-         
-    }
-}
+//struct Tippy: Tip {
+//    var title: Text {
+//        Text("Tippyity")
+//    }
+//    var message: Text? {
+//        Text("Add new tip")
+//    }
+//    var image: Image? {
+//        Image(systemName: "paintpalette")
+//    }
+//    
+//    var rules: [Rule] {
+//         
+//    }
+//}
 
 //pop
 struct DeleteConstellation: Tip {
@@ -33,7 +33,7 @@ struct DeleteConstellation: Tip {
         Text("Press and hold to delete constellation")
     }
     var image: Image? {
-        Image(systemName: "paintpalette")
+        Image(systemName: "trash")
     }
     
     var rules: [Rule] {
@@ -47,16 +47,49 @@ struct EditEquation: Tip {
         Text("Edit Equation")
     }
     var message: Text? {
-        Text("Press on an equation to edit it")
+        Text("Tap on an equation to edit it")
     }
     var image: Image? {
-        Image(systemName: "paintpalette")
+        Image(systemName: "hand.tap")
     }
     
     var rules: [Rule] {
          
     }
 }
+
+struct StarCoordinates: Tip {
+    var title: Text {
+        Text("Edit Equation")
+    }
+    var message: Text? {
+        Text("Tap on a star to see its coordinates")
+    }
+    var image: Image? {
+        Image(systemName: "star.fill")
+    }
+    
+    var rules: [Rule] {
+         
+    }
+}
+
+struct AddEquation: Tip {
+    var title: Text {
+        Text("Add New Equation")
+    }
+    var message: Text? {
+        Text("Press this button to add a new equation to the constellation")
+    }
+    var image: Image? {
+        Image(systemName: "pencil.and.scribble")
+    }
+    
+    var rules: [Rule] {
+         
+    }
+}
+
 
 
 //always initialise the tip like let ... = ...
